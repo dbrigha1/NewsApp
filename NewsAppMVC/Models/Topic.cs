23dcf1e2495aeec33a998998e973dd1774e5bca9
Dylan A Brigham <dbrigha1@uwyo.edu>
2018-01-10 20:27:56 -0700

    Adding multiple articles to topics

--- NewsAppMVC/Models/Topic.cs
+++ NewsAppMVC/Models/Topic.cs
@@ -8,13 +8,17 @@ namespace NewsAppMVC.Models
 {
     public class Topic
     {
+        public Topic()
+        {
+            this.Articles = new HashSet<Article>();
+        }
         public int ID { get; set; }
         public string Name { get; set; }
         [Display(Name = "Date Time")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateCreated { get; set; }
-        public virtual ICollection<Article> Article { get; set; }
+        public virtual ICollection<Article> Articles { get; set; }
         
     }
 }
